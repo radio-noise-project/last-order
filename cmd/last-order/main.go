87abe33ab97466a5f0c8012e525f26c2ff5882fd
Aoi Kondo <agotadmidori@gmail.com>
2024-11-06 21:31:06 +0900

    feat: Add scripts of Echo server

--- cmd/last-order/main.go
+++ cmd/last-order/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+
+	"github.com/radio-noise-project/last-order/internal/api"
+)
 
 func main() {
 	slog.Info("Start Last-Order")
+	api.Server()
 }
