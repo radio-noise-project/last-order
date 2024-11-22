a2ce8385aee4876e185f44f9cf0752eb3da54592
Aoi Kondo <agotadmidori@gmail.com>
2024-11-22 20:05:12 +0900

    feat: Add the utility functions

---
+++ internal/api/handler/types.go
@@ -0,0 +1,25 @@
+package handler
+
+import "time"
+
+type VersionInformationSisters struct {
+	CodeName            string    `json:"codeName"`
+	Version             string    `json:"version"`
+	GolangVersion       string    `json:"golangVersion"`
+	DockerEngineVersion string    `json:"dockerEngineVersion"`
+	BuiltGitCommitHash  string    `json:"builtgitCommitHash"`
+	BuiltDate           time.Time `json:"builtDate"`
+	Os                  string    `json:"os"`
+	Arch                string    `json:"arch"`
+}
+
+type VersionInformationLastOrder struct {
+	CodeName            string    `json:"codeName"`
+	Version             string    `json:"version"`
+	GolangVersion       string    `json:"golangVersion"`
+	DockerEngineVersion string    `json:"dockerEngineVersion"`
+	BuiltGitCommitHash  string    `json:"builtgitCommitHash"`
+	BuiltDate           time.Time `json:"builtDate"`
+	Os                  string    `json:"os"`
+	Arch                string    `json:"arch"`
+}
