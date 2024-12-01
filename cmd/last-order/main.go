72f5b430f985ef8fae0d8c96fed725fb96073c02
Aoi Kondo <agotadmidori@gmail.com>
2024-12-01 09:08:37 +0900

    fix: Change endpoint so as to follow changing directory structure

--- cmd/last-order/main.go
+++ cmd/last-order/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"log/slog"
 
-	"github.com/radio-noise-project/last-order/internal/api"
+	"github.com/radio-noise-project/last-order/internal/api/server"
 )
 
 func main() {
 	slog.Info("Start Last-Order")
-	api.Router()
+	server.Start()
 }
